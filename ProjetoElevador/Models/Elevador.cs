6b1c6884a0269dea6d931c98e43040ee5f42f5d1
AlekSantos <alekssandersantos@gmail.com>
2022-05-25 17:29:12 -0300

    1

---
+++ ProjetoElevador/Models/Elevador.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoElevador.Models
+{
+    internal class Elevador{
+        public int andarAtual;
+        public int totalDeAndares;
+        public int capacidadeElevador;
+        public int quantidadeDePessoas;
+
+        public void Inicializar()
+        {
+        
+            Console.WriteLine("Digite a capacidade do elevador");
+            capacidadeElevador = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o total de andares do predio");
+            totalDeAndares = int.Parse(Console.ReadLine());
+
+        }
+        public void Entrar()
+        {
+            if (quantidadeDePessoas < capacidadeElevador)
+            {
+                quantidadeDePessoas= quantidadeDePessoas +1;
+                Console.WriteLine("Existem " +quantidadeDePessoas + " pessoas no elevador");
+
+            }
+            else
+            {
+                Console.WriteLine("Existem " + quantidadeDePessoas + " pessoas no elevador,limite esgotado");
+            }
+
+        }
+        public void Sair()
+        {
+            if (quantidadeDePessoas > 0)
+            {
+                quantidadeDePessoas = quantidadeDePessoas - 1;
+                Console.WriteLine("Existem " + quantidadeDePessoas + " pessoas no elevador");
+
+            }
+            else
+            {
+                Console.WriteLine("Não tem ninguem no elevador ");
+            }
+        }
+        public void Subir()
+        {
+            if (andarAtual < totalDeAndares)
+            {
+                andarAtual = andarAtual + 1;
+                Console.WriteLine("Voce esta no " + andarAtual + " andar");
+            }
+            else
+            {
+                Console.WriteLine("Voce esta no ultimo andar");
+            }
+        }
+        public void Descer()
+        {
+            if (andarAtual > 0)
+            {
+                andarAtual = andarAtual - 1;
+                Console.WriteLine("Voce esta no " + andarAtual + " andar");
+
+            }
+            else
+            {
+                Console.WriteLine("Voce esta no Terreo");
+            }
+        }
+
+
+    }
+}
---
+++ ProjetoElevador/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using ProjetoElevador.Models;
+
+namespace ProjetoElevador
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Boolean continuar = true;
+            Console.WriteLine("Bem vindo ao Elevador");
+            Elevador elevadornovo = new Elevador();
+            elevadornovo.Inicializar();
+            while (continuar != false)
+            {
+               
+                Console.WriteLine(@"Escolha uma opção:
+                                      1 - Entrar no elevador
+                                      2 - Sair do elavador
+                                      3 - Subir um andar
+                                      4 - Descer um andar
+                                      5 - Sair
+                          ");
+                string escolhar = Console.ReadLine();
+                Console.Clear();
+
+                switch (escolhar)
+                {
+                    case "1":
+                        elevadornovo.Entrar();  
+                        break;
+                    case "2":
+                        elevadornovo.Sair();
+                        break;
+                    case "3":
+                        elevadornovo.Subir();
+                        break;
+                    case "4":
+                        elevadornovo.Descer();
+                        break;
+                    case "5":
+                        continuar = false;
+                        break;
+
+                    default:
+                        Console.WriteLine("escolha invalida");
+                        break;
+
+                }
+
+
+            }
+        }
+    }
+}
